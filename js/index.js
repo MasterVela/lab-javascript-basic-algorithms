6891742386075bb753528db2df5cc00089babd48
Sergi Vela <sergivelamoreno@Sergis-MacBook-Air.local>
2022-07-05 21:29:53 +0200

    firstIteration

--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
 // Iteration 1: Names and Input
 //
 // Iteration 2: Conditionals
-
-
+//
 // Iteration 3: Loops
+
+let hacker1 = "JOHN"
+console.log("The driver's name is " + hacker1)
+let hacker2 = "MARIA"
+console.log("The navigator's name is " + hacker2)
+
